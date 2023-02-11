44f83260eb256dcbb62a8a4d8f27c651cc814e29
Djoufson <djouflegran@gmail.com>
2023-02-10 19:50:09 -0800

    Fixing InboxPage responsive issues

---
+++ ChatApp.Mobile/Converters/ColumnConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace ChatApp.Mobile.Converters;
+
+public class ColumnConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string fromUserEmail)
+            return (fromUserEmail.Equals(Preferences.Get(Constants.USER_EMAIL_KEY, ""))) ?
+                1 :
+                0;
+
+        return 1;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return null;
+    }
+}
